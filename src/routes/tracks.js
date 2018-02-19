512dc9fb1f10bc3b7eae9413ccca5b7e43011155
Daniel <daniel.daeschle@gmail.com>
2018-02-19 23:19:34 +0100

    added docs and route

--- src/routes/tracks.js
+++ src/routes/tracks.js
@@ -7,6 +7,9 @@ const { generateTrackingNumber } = require('../lib');
 const router = express.Router();
 
 router.route('/api/tracks/:trackingId')
+  /**
+   * Returns on track by tracking id
+   */
   .get((req, res) => {
     let id = req.params.trackingId;
 
@@ -40,6 +43,9 @@ router.route('/api/tracks/:trackingId')
       }
     });
   })
+  /**
+   * Creates a track state by tracking id
+   */
   .post((req, res) => {
     let id = req.params.trackingId;
 
@@ -81,51 +87,87 @@ router.route('/api/tracks/:trackingId')
     }
   });
 
-router.post('/api/tracks', (req, res) => {
-  // Validate payload
-  let {error, value} = Joi.validate(req.body, TrackSchema);
+router.route('/api/tracks')
+  /**
+   * Creates a track (for staff members)
+   * Contains no authorization yet
+   */
+  .post((req, res) => {
+    // Validate payload
+    let {error, value} = Joi.validate(req.body, TrackSchema);
 
-  // Check if validation is successful
-  if (!error) {
-    value.trackingNumber = generateTrackingNumber(
-      value.senderPostCode,
-      value.receiverPostCode,
-      value.date,
-      value.packageSize,
-      value.isExpress
-    );
+    // Check if validation is successful
+    if (!error) {
+      value.trackingNumber = generateTrackingNumber(
+        value.senderPostCode,
+        value.receiverPostCode,
+        value.date,
+        value.packageSize,
+        value.isExpress
+      );
 
-    // Create object or return error
+      // Create object or return error
+      Track
+        .findOrCreate({
+          where: value,
+          defaults: {
+            trackingNumber: value.trackingNumber
+          }
+        })
+        .spread((track, created) => {
+          if (created) {
+            // On saved
+            res.json({
+              data: {
+                trackingNumber: track.trackingNumber,
+                senderPostCode: track.senderPostCode,
+                receiverPostCode: track.receiverPostCode,
+                date: track.date,
+                packageSize: track.packageSize,
+                isExpress: track.isExpress,
+                packageStates: []
+              },
+              error: false
+            });
+          } else {
+            // Return error
+            res.status(400).json({ message: 'Track already exists', error: true });
+          }
+        });
+    } else {
+      // Returns {errors: [...]}
+      res.status(400).json({ error: true, message: error.details.map(it => it.message)[0] });
+    }
+  })
+  /**
+   * Returns all tracks for staff members
+   * Contains no authorization yet
+   */
+  .get((req, res) => {
     Track
-      .findOrCreate({
-        where: value,
-        defaults: {
-          trackingNumber: value.trackingNumber
-        }
+      .findAll({
+        include: { model: PackageState, as: 'packageStates' }
       })
-      .spread((track, created) => {
-        if (created) {
-          // On saved
-          res.json({
-            data: {
-              trackingNumber: track.trackingNumber,
-              senderPostCode: track.senderPostCode,
-              receiverPostCode: track.receiverPostCode,
-              date: track.date,
-              packageSize: track.packageSize,
-              isExpress: track.isExpress,
-            },
-            error: false
-          });
-        } else {
-          // Return error
-          res.status(400).json({ message: 'Track already exists', error: true });
-        }
+      .then(data => {
+        let tracks = data.map(track_ => {
+          let track = track_.dataValues;
+          return {
+            trackingNumber: track.trackingNumber,
+            senderPostCode: track.senderPostCode,
+            receiverPostCode: track.receiverPostCode,
+            date: track.date,
+            packageSize: track.packageSize,
+            isExpress: track.isExpress,
+            packageStates: track.packageStates.map(state => state.dataValues)
+          };
+        });
+        res.json({
+          data: {
+            tracks
+          },
+          error: false
+        });
       });
-  } else {
-    // Returns {errors: [...]}
-    res.status(400).json({ error: true, message: error.details.map(it => it.message)[0] });
-  }
-});
+  });
 
 module.exports = router;
